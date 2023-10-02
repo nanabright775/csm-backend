63ce21cc28776b8f903bc21727eecfcef3378590
nanabright775 <nanabrightatsiatorme@gmail.com>
2023-10-02 18:44:14 +0600

    secret key taken cared of

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$a%5vvx%7d6m$tm8@!-uppj-jkv6(i8w3_#c-vc&z+xrl(=s84'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
